1d383fe81761d96bb4eaf15b1f2a73b4f2a0ab17
Luis Pamplona <luispamplona2702@gmail.com>
2023-03-28 23:24:34 -0300

    Criado site que muda a cor do plano de fundo

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+function vermelho(){
+
+    var corpo = window.document.getElementById("corpo")
+
+    corpo.style.background="red"
+
+}
+
+function verde(){
+
+    var corpo = window.document.getElementById("corpo")
+    
+    corpo.style.background="green"
+
+}
+
+function azul(){
+
+    var corpo = window.document.getElementById("corpo")
+
+    corpo.style.background="blue"
+
+}
+
+function reset(){
+
+    var corpo = window.document.getElementById("corpo")
+
+    corpo.style.background="white"
+
+}
